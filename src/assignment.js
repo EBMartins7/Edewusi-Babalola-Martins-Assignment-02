bc299c8601fe5fe9b954e6a82cc881b754f9b139
Edewusi Babalola Martins <99411579+EBMartins7@users.noreply.github.com>
2022-07-15 00:53:36 +0000

    check challenge 1 check challenge 2

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,16 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let sum = 0; //variable holding the sum of all numbers in array
+
+  for (let i = 0; i <= arrayOfNumbers.length - 1; i++) {
+    sum += arrayOfNumbers[i]
+  }
+  return sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+
+
+ assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +35,20 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let countEvenNumbers = 0;
+  for(let i = 0; i <= arrayOfNumbers.length - 1; i++) {
+    if (arrayOfNumbers[i] % 2 == 0) {
+      countEvenNumbers = countEvenNumbers + 1;
+    }
+    else {
+      countEvenNumbers = countEvenNumbers + 0;
+    }
+  }
+    return countEvenNumbers;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+
+assignment.countEvenNumbers = countEvenNumbers;
+
 
 /**
  * Challenge - 3
